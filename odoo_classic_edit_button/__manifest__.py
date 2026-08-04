a76cb66dfa4d2589f152e1c28054692f705f2eb4
hail-hydra <hamzbond@gmail.com>
2026-08-04 11:42:04 +0700

    update Hamzbond to hamzbond

--- odoo_classic_edit_button/__manifest__.py
+++ odoo_classic_edit_button/__manifest__.py
@@ -1,8 +1,8 @@
 {
     'name': 'Restore Classic Form Edit Button',
     'version': '19.0.1.0.0',
-    'author': 'Hamzbond',
-    'maintainer': 'Hamzbond',
+    'author': 'hamzbond',
+    'maintainer': 'hamzbond',
     'summary': 'Restores the classic Edit button in form views, allowing users to switch between read-only and edit modes.',
     'description': 'This module restores the classic Edit button in form views, allowing users to switch between read-only and edit modes. It provides a familiar interface for users who prefer the traditional form editing experience.',
     'category': 'Technical',
